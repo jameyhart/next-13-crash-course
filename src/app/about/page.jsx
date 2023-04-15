0c3c505fba424e9784e4b8edd33cb1d9d89eb2de
Jamey Hart <jamey.hart@jisc.ac.uk>
2023-04-15 16:33:22 +0100

    Pages

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
 const AboutPage = () => {
-    return <div>AboutPage</div>;
+    return <div>
+        <h1>About</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat, neque. Magni placeat eum, vitae provident consequuntur beatae accusamus dolores accusantium rerum non nisi quam voluptas, fugit doloribus quas quia laudantium!</p>
+    </div>;
 };
 
 export default AboutPage;
---
+++ src/app/about/team/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TeamPage = () => {
+    return (
+        <div>
+            <h1>Team</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam perspiciatis itaque magnam incidunt natus totam, corrupti quae? Magnam, placeat qui commodi voluptatem officia ducimus, sed necessitatibus saepe possimus unde rerum.</p>
+        </div>
+    )
+}
+
+export default TeamPage
