2d0a86d6f2e0f46a304364bbd43cf52e9ab1a110
Jamey Hart <jamey.hart@jisc.ac.uk>
2023-04-17 04:44:39 +0100

    Icons

--- src/app/code/repos/page.tsx
+++ src/app/code/repos/page.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Link from 'next/link';
+import { FaStar, FaCodeBranch, FaEye } from 'react-icons/fa';
 
 const fetchRepos = async () => {
     const res = await fetch('https://api.github.com/users/jameyhart/repos');
@@ -10,8 +12,33 @@ const ReposPage = async () => {
     const repos = await fetchRepos();
 
     return (
-        <div>
-            {repos[0].name}
+        <div className='repos-container'>
+            <h2>Repos</h2>
+                <ul className='repo-list'>
+                    {repos.map((repo) => (
+                        <li key={repo.id}>
+                            <Link href={`/code/repos/${repo.name}`}>
+                                <h3>
+                                    {repo.name}
+                                </h3>
+                                <p>
+                                    {repo.description}
+                                </p>
+                                <div className='repo-details'>
+                                    <span>
+                                        <FaStar /> {repo.stargazers_count}
+                                    </span>
+                                    <span>
+                                        <FaCodeBranch /> {repo.forks_count}
+                                    </span>
+                                    <span>
+                                        <FaEye /> {repo.watchers_count}
+                                    </span>
+                                </div>
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
         </div>
     )
 };
