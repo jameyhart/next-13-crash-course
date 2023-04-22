c8820c92695eb945b1f0b500d5859c5a7a6eaf2a
Jamey Hart <jamey.hart@jisc.ac.uk>
2023-04-22 15:58:58 +0100

    Add courses

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,15 @@
+// @ts-nocheck
 import React from "react";
-import Link from "next/link";
+
+import Courses from "./components/Courses";
 
 const HomePage = () => {
-    return <div>
-        <h1>Welcome to Traversy Media</h1>
-        <ul>
-            <li>
-                <Link href="/">
-                    Home
-                </Link>
-            </li>
-            <li>
-                <Link href="/about">
-                    About
-                </Link>
-            </li>
-            <li>
-                <Link href="/about/team">
-                    Team
-                </Link>
-            </li>
-        </ul>
-    </div>;
+    return (
+        <>
+            <h1>Welcome to Traversy Media</h1>
+            <Courses />
+        </>
+    );
 };
 
 export default HomePage;
