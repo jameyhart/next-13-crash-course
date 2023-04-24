12f8120bfe05770bd1b980301010fe8fa85081ef
Jamey Hart <jamey.hart@jisc.ac.uk>
2023-04-24 08:33:37 +0100

    Init create course endpoint

--- src/app/api/courses/route.ts
+++ src/app/api/courses/route.ts
@@ -5,3 +5,16 @@ import courses from './data.json';
 export const GET = async () => {
     return NextResponse.json(courses);
 };
+
+export const POST = async (request: Request) => {
+    const {
+        title,
+        description,
+        level,
+        link
+    } = await request.json();
+
+    console.log(title, description, level, link);
+
+    return NextResponse.json({ message: 'Course created.' });
+};
